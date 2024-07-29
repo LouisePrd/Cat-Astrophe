68e1b0d34ee4bab19d42dfa6752c32ed2c39e165
LouisePrd <louise.peridy22@gmail.com>
2024-07-29 23:55:22 +0200

    ajout router

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,13 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+
 let message = 'douteux :)';
 let name = ref('');
 let showGreeting = ref(false);
 let showInput = ref(true);
 
 const addName = () => {
-  if (name) {
+  if (name.value.trim()) {
     showGreeting.value = true;
     showInput.value = false;
   }
@@ -35,12 +36,101 @@ const addName = () => {
     </div>
     <div>
       <input type="radio" id="mood-oui-rose" name="mood" value="oui-rose" />
-      <label for="mood-oui-rose">Oui en rose</label>
+      <label id="mood-oui-rose" for="mood-oui-rose">Oui en rose</label>
     </div>
-    <p>Clique sur l'image pour commencer. (stp)</p>
+    <p>Clique sur l'image pour commencer (stp)</p>
     <img class="charming-guy" src="/charming-guy.jpg" alt="Un charmant jeune homme" />
   </div>
 
 </template>
 
-<style scoped></style>
+<style scoped>
+html {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+
+h1 {
+  font-family: var(--font-title);
+  color: var(--primary-color);
+  text-align: center;
+  font-size: var(--font-size-large);
+}
+
+h2 {
+  font-family: var(--font-title);
+  color: var(--primary-color);
+  text-align: center;
+  font-size: var(--font-size-small);
+}
+
+@keyframes fadeIn {
+  from {
+    opacity: 0;
+  }
+
+  to {
+    opacity: 1;
+  }
+}
+
+p {
+  font-family: var(--font-text);
+  color: var(--secondary-color);
+  text-align: center;
+  font-size: var(--font-size-small);
+}
+
+.name {
+  font-family: var(--font-text);
+  font-size: var(--font-size-xsmall);
+  color: var(--secondary-color);
+  background-color: transparent;
+  border: 2px solid var(--secondary-color);
+  border-radius: 5px;
+  padding: 0.4rem;
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+}
+
+.shame {
+  animation: fadeIn 10s;
+  font-size: var(--font-size-xxsmall);
+}
+
+.start {
+  font-family: var(--font-text);
+  font-size: var(--font-size-xsmall);
+  color: var(--secondary-color);
+  background-color: transparent;
+  border: 2px solid var(--secondary-color);
+  border-radius: 15px;
+  padding: 0.4rem;
+  display: block;
+  margin: 0 auto;
+  text-align: center;
+  margin-top: 1rem;
+  cursor: pointer;
+}
+
+.greetings-form {
+  font-family: var(--font-text);
+  font-size: var(--font-size-xsmall);
+  color: var(--secondary-color);
+  padding: 0.4rem;
+  text-align: center;
+}
+
+#mood-oui-rose {
+  color: rgb(234, 0, 247);
+}
+
+.charming-guy {
+  max-width: 10%;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import './assets/main.css'
-import './assets/style-home.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(router);
+app.mount('#app');
