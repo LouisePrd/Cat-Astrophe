92734280bdccf200fac9c98c6761b7dfcea5c74e
LouisePrd <louise.peridy22@gmail.com>
2024-07-31 12:26:40 +0200

    ajout composant musique

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <script setup lang="ts">
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
+import MusicPlayer from '@/components/MusicPlayer.vue';
+
 </script>
 
 <template>
   <Header />
   <router-view></router-view>
-
+  <MusicPlayer />
   <Footer />
 </template>
 
