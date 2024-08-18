a5d8d8fa160d9f23041d00522d933329655dd69e
LouisePrd <louise.peridy22@gmail.com>
2024-08-18 20:15:37 +0200

    ajout update DB Tom

--- src/components/Duck.vue
+++ src/components/Duck.vue
@@ -3,6 +3,12 @@ import { ref } from 'vue';
 import { supabase } from '@/lib/supabaseClient';
 
 let tabDuck = ref<any>([]);
+document.title = 'Mascotte';
+let nourrir = ref('false');
+let jouer = ref('false');
+let donnerABoire = ref('false');
+let mettreAuLit = ref('false');
+let soigner = ref('false');
 
 const getDataDuck = async () => {
     try {
@@ -19,36 +25,66 @@ const getDataDuck = async () => {
     }
 };
 
+const updateDataDuckDB = async () => {
+    try {
+        const { data, error } = await supabase
+            .from('duck')
+            .update(tabDuck.value)
+            .eq('name', tabDuck.value.name);
+        if (error) {
+            throw error;
+        }
+    } catch (error) {
+        console.error(error);
+    }
+};
+
 const updateDataDuck = async (event: MouseEvent) => {
     if (event && event.target) {
         const target = event.target as HTMLElement;
         switch (target.textContent) {
             case 'Nourrir':
-                if (tabDuck.value.hunger > 0 && tabDuck.value.hunger < 20) {
+                if (tabDuck.value.hunger > 0 && tabDuck.value.hunger < 20 && nourrir.value === 'false') {
                     tabDuck.value.hunger -= 1;
+                    nourrir.value = 'true';
+                } else if (nourrir.value === 'true') {
+                    alert('Vous avez déjà nourri" + tabDuck.name + " !');
                 }
                 break;
             case 'Jouer':
-                if (tabDuck.value.happiness > 0 && tabDuck.value.happiness < 20) {
+                if (tabDuck.value.happiness > 0 && tabDuck.value.happiness < 20 && jouer.value === 'false') {
                     tabDuck.value.happiness += 1;
+                    jouer.value = 'true';
+                } else if (jouer.value === 'true') {
+                    alert('Vous avez déjà joué avec " + tabDuck.name + " !');
                 }
                 break;
             case 'Donner à boire':
-                if (tabDuck.value.thirst > 0 && tabDuck.value.thirst <= 20) {
+                if (tabDuck.value.thirst > 0 && tabDuck.value.thirst <= 20 && donnerABoire.value === 'false') {
                     tabDuck.value.thirst -= 1;
+                    donnerABoire.value = 'true';
+                } else if (donnerABoire.value === 'true') {
+                    alert('Vous avez déjà donné à boire à " + tabDuck.name + " !');
                 }
                 break;
             case 'Mettre au lit':
-                if (tabDuck.value.fatigue > 0 && tabDuck.value.fatigue < 20) {
+                if (tabDuck.value.fatigue > 0 && tabDuck.value.fatigue < 20 && mettreAuLit.value === 'false') {
                     tabDuck.value.fatigue -= 1;
+                    mettreAuLit.value = 'true';
+                } else if (mettreAuLit.value === 'true') {
+                    alert('Vous avez déjà mis au lit " + tabDuck.name + " !');
                 }
                 break;
             case 'Soigner':
-                if (tabDuck.value.health > 0 && tabDuck.value.health < 20) {
+                if (tabDuck.value.health > 0 && tabDuck.value.health < 20 && soigner.value === 'false') {
                     tabDuck.value.health += 1;
+                    soigner.value = 'true';
+                } else if (soigner.value === 'true') {
+                    alert('Vous avez déjà soigné " + tabDuck.name + " !');
                 }
                 break;
         }
+        updateDataDuckDB();
     }
 };
 
